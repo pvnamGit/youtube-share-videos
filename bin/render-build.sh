dd8958d9fa0ab3f56f585b321cb49f99de46b12a
pvnamGit <pvnam.forwork@gmail.com>
2023-08-16 13:21:57 +0700

    fix NODE_ENV value

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+# Set NODE_ENV to development temporarily to install devDependencies
+export NODE_ENV=development
+
 # install JavaScript dependencies
-yarn install
+yarn install --production=false
 
 bundle install
 bundle exec rake assets:precompile
@@ -10,3 +13,6 @@ bundle exec rake assets:clean
 bundle exec rake db:migrate
 
 ./bin/webpack
+
+# Optional: Set NODE_ENV back to production if required
+export NODE_ENV=production
